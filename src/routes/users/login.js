5a366e98077465969d887a99baa7e4edb7de3127
Jeff Taylor-Chang <jeff@jefftc.com>
2020-03-19 15:19:39 -0400

    Added email verification to login route

--- src/routes/users/login.js
+++ src/routes/users/login.js
@@ -1,6 +1,19 @@
 import middyfy from 'middleware';
 
+import { verifyEmail } from 'utils/auth';
+
 const handler = async (event, context) => {
+  const verifiedEmail = await verifyEmail(event.body?.user?.email);
+
+  if (!verifiedEmail.success) {
+    return {
+      statusCode: 401,
+      body: {
+        message: verifiedEmail.error?.message
+      }
+    };
+  }
+
   return {
     statusCode: 200,
     body: {
