cf5e2b6dc5f41daa63d9b4f8960f61db84c7f44f
Jeff Taylor-Chang <jeff@jefftc.com>
2020-03-23 15:51:52 -0400

    Updated routes

---
+++ src/routes/excuse/approve.js
@@ -0,0 +1,23 @@
+import middyfy from 'middleware';
+import createHttpError from 'http-errors';
+
+const handler = async (event, context) => {
+  if (!event.authorized || !event.user.privileged) {
+    throw new createHttpError.Unauthorized('Not authorized');
+  }
+
+  // TODO
+
+  return {
+    statusCode: 200,
+    body: {
+      message: 'Hello World'
+    }
+  };
+};
+
+export default middyfy(handler, {
+  authorized: true,
+  useMongo: true,
+  useSql: true
+});
