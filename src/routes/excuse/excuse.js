3eaddd06d447268908c050d5529c2f6a88638f65
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-05 14:55:03 -0400

    Added excuse creation endpoint

--- src/routes/excuse/excuse.js
+++ src/routes/excuse/excuse.js
@@ -1,17 +1,44 @@
 import middyfy from 'middleware';
 import createHttpError from 'http-errors';
+import oc from 'js-optchain';
+
+import { createExcuse } from 'services/event';
+import { extractNetid } from 'services/user';
 
 const _handler = async (event, context) => {
   if (!event.authorized) {
     throw new createHttpError.Unauthorized('Not authorized');
   }
 
-  // TODO
+  const ocBody = oc(event.body, {
+    excuse: {
+      event_id: '',
+      reason: ''
+    }
+  });
+
+  if (ocBody.excuse.event_id === '' || ocBody.excuse.reason === '') {
+    throw new createHttpError.BadRequest('Missing required fields');
+  }
+
+  let newExcuse = {
+    ...ocBody.excuse,
+    netid: extractNetid(event.user.email)
+  };
+
+  const createdExcuse = await createExcuse(newExcuse);
+
+  if (!createdExcuse.success) {
+    throw new createHttpError.InternalServerError('Could not create excuse');
+  }
 
   return {
     statusCode: 200,
     body: {
-      message: 'Hello World'
+      excuse: {
+        ...newExcuse,
+        approved: 0
+      }
     }
   };
 };
