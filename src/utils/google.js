6cee4f7ba927c3533831eff673d3dbd5b1c77716
Jeff Taylor-Chang <jeff@jefftc.com>
2020-04-13 09:38:47 -0400

    Added ios standalone client id to google service

--- src/utils/google.js
+++ src/utils/google.js
@@ -1,39 +1,32 @@
 import { OAuth2Client } from 'google-auth-library';
+import { pass, fail } from 'utils/res';
 
 export const verifyToken = async (token, email) => {
   try {
-    const client = new OAuth2Client('223233671218-ceilcecpn0t04ec5or3tk680pfoomf4v.apps.googleusercontent.com');
+    const client = new OAuth2Client('223233671218-v5meaa316pd8mgar3mgcvsmg3td7qnl6.apps.googleusercontent.com');
 
     const ticket = await client.verifyIdToken({
       idToken: token,
       audience: [
         '223233671218-ceilcecpn0t04ec5or3tk680pfoomf4v.apps.googleusercontent.com',
-        '223233671218-joevmt53u95c0o70mttjrodcbd5nj23j.apps.googleusercontent.com'
+        '223233671218-joevmt53u95c0o70mttjrodcbd5nj23j.apps.googleusercontent.com',
+        '223233671218-v5meaa316pd8mgar3mgcvsmg3td7qnl6.apps.googleusercontent.com'
       ]
     });
 
     const payload = ticket.getPayload();
 
     if (payload.email.toLowerCase() !== email.toLowerCase()) {
-      return {
-        success: false,
-        error: {
-          message: 'email mismatch'
-        }
-      };
+      return fail({
+        message: 'email mismatch'
+      });
     }
 
-    return {
-      success: true,
-      data: {
-        familyName: payload.family_name,
-        givenName: payload.given_name
-      }
-    };
+    return pass({
+      familyName: payload.family_name,
+      givenName: payload.given_name
+    });
   } catch (error) {
-    return {
-      success: false,
-      error
-    };
+    return fail(error);
   }
 };
